9b70ace62665bdc6714e0ca9ecd3e6ca04471bcb
firatkaanbitmez <firatbitmez@gmail.com>
2023-11-30 22:01:26 +0300

    ders3

---
+++ Ders3_30_11/DerseHazirlik/index.js
@@ -0,0 +1,39 @@
+/*Javascript Nedir?
+Javascript geliştiricilerin etkileşimli web sayfaları oluşturmak için kullandığı bir programla dilidir.
+ */
+//Fırat
+/* FIRAT
+ KAAN BİTMEZ*/
+
+console.log(
+  "firat"
+); /* terminalae  node index.js   yazdığımızda firat    diye çıktı verir */
+
+//const, let ,var  DEĞİŞKENLER
+//const değiştirilemez değişken
+//
+//
+let name = "firat bitmez";
+name = "test";
+console.log(name);
+const age = 27; /*bir sayı(Yani Number olarak) tanımlamak istersem tırnaksız tanımlarım sayı olarak tanımlarım eğer tırnak içinde tanımlarsam string olur yani text olur */
+console.log(age);
+console.log(name, age);
+
+const result = true; /* true false  tanımlama boolean ifade*/
+
+console.log(
+  typeof result
+); /*typeof dan sonra gelen ifadenin bana string olarak verilmesini istiyorum demek */
+
+const numbeer = 10;
+const numbeer1 = "10";
+
+console.log("TOPLAMA", age + numbeer); /*çıktııs TOPLAMA 37 */
+console.log("TOPLAMA", age + numbeer1); /*çıktısı TOPLAMA 2710 */
+
+
+
+//null  bombış ifade
+//undefined tanımsız ifadelerde kullanılır
+
