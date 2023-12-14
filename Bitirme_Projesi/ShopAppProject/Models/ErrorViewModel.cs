01a0b9997724edb58f9517f29314a925c3c40f98
firatkaanbitmez <74864221+firatkaanbitmez@users.noreply.github.com>
2023-12-15 00:53:12 +0300

    add

--- Bitirme_Projesi/ShopAppProject/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ShopAppProject.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
